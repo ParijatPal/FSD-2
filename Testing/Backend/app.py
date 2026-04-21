de023b5adabd45970049d9aa8ff8293bbd5ae57a
Parijat Pal <parijatpal.job@gmail.com>
2026-04-21 22:37:14 +0530

    Update backend Docker setup Exp20

--- Testing/Backend/app.py
+++ Testing/Backend/app.py
@@ -14,3 +14,5 @@ app = create_app()
 @app.route("/")
 def home():
     return {"message": "Backend Server is running"}
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
